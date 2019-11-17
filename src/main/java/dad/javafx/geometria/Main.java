1ac62f5b93f2b434f2dedce0e198258b09a744fd
Bukitei <borjagomezb@gmail.com>
2019-11-17 20:14:28 +0100

    terminado

---
+++ src/main/java/dad/javafx/geometria/Main.java
@@ -0,0 +1,10 @@
+package dad.javafx.geometria;
+
+public class Main {
+
+    //Este es del ejercicio en clase
+	public static void main(String[] args) {
+		GeometriaApp.main(args);
+	}
+
+}
---
+++ src/main/java/dad/javafx/main/Main.java
@@ -0,0 +1,12 @@
+package dad.javafx.main;
+
+public class Main {
+
+    
+    //Este es el que se ejecutará
+	public static void main(String[] args) {
+		GeometriaApp.main(args);
+
+	}
+
+}
